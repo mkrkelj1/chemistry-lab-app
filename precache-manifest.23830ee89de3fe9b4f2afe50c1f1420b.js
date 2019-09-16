42375f1842442033db080854e07773c02939d69a
Marko Krkeljas <mkrkelja@IMATS-X4084JG5L.local>
2019-09-16 11:20:37 -0400

    Updates

---
+++ precache-manifest.23830ee89de3fe9b4f2afe50c1f1420b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef740cb15ac22caf3ecfe13a6fd8b4ae",
+    "url": "/chemistry-lab-app/index.html"
+  },
+  {
+    "revision": "783bd076be4fc24ab521",
+    "url": "/chemistry-lab-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b37ba4967e16459932aa",
+    "url": "/chemistry-lab-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "783bd076be4fc24ab521",
+    "url": "/chemistry-lab-app/static/js/2.fd197feb.chunk.js"
+  },
+  {
+    "revision": "b37ba4967e16459932aa",
+    "url": "/chemistry-lab-app/static/js/main.13ddbe49.chunk.js"
+  },
+  {
+    "revision": "f2991d8b8eea07680d14",
+    "url": "/chemistry-lab-app/static/js/runtime~main.e9f9f155.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/chemistry-lab-app/precache-manifest.23830ee89de3fe9b4f2afe50c1f1420b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chemistry-lab-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
