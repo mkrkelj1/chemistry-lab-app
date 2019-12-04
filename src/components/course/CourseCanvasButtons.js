95fd226c1586ac7794a443c0295d3d84bcce1106
kvwenzel <55603758+kvwenzel@users.noreply.github.com>
2019-12-04 14:58:12 -0500

    adjust to CSS styling guide; testing class time instead of section number

--- src/components/course/CourseCanvasButtons.js
+++ src/components/course/CourseCanvasButtons.js
@@ -3,12 +3,22 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
-
+/*
 const sections = [1, 2, 3, 4, 5, 6]
 
 const cols = sections.map(section => (
   <Col xs={4} md={2}><Button variant = "primary"  >Section {section}</Button></Col>
 ));
+*/
+
+/*testing hours...
+since students may not know their section number...
+Ideally I think it should say "Section 1" and on hover reveal the time of the section*/
+const sections = ["10:00-10:50", "11:00-11:50", "12:00-12:50", "01:00-01:50", "02:00-02:50", "03:00-03:50"]
+const cols = sections.map(section => (
+  <Button variant = "primary"  >{section}</Button>
+));
+
 
 const CourseCanvasButtons = () => {
   return (
@@ -22,4 +32,3 @@ const CourseCanvasButtons = () => {
 };
 
 export default CourseCanvasButtons;
-
