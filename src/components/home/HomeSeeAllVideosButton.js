c07b3f6c0d5017c8d386168c9442c8b40118c55b
kvwenzel <55603758+kvwenzel@users.noreply.github.com>
2019-12-04 12:44:53 -0500

    Adjust padding for See All Videos button

--- src/components/home/HomeSeeAllVideosButton.js
+++ src/components/home/HomeSeeAllVideosButton.js
@@ -4,11 +4,15 @@ import Container from "react-bootstrap/Container";
 
 
 const HomeSeeAllVideosButton = () => (
+  
     <Container className="p-1">
+      <div style={{display: 'flex', justifyContent: 'center'}}>
       <Button variant="secondary" href="#" block>
         See All Video Tutorials
       </Button>
+      </div>
     </Container>
+  
 );
 
 export default HomeSeeAllVideosButton;
